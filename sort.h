24da26c691ebd758436757eab7e1cea79a54e906
NeutronAhmed <bazubairu@st.ug.edu.gh>
2023-07-01 22:22:45 -0700

    work

--- sort.h
+++ sort.h
@@ -26,4 +26,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 int partition(int *array, int lo, int hi, size_t size);
 void quick_s(int *array, int lo, int hi, size_t size);
+void shell_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 #endif
